c23b908fde8c308d9a6fc5b6b19f2898a6c1e55a
luketheshack <44590978+luketheshack@users.noreply.github.com>
2021-01-24 15:36:48 -0500

    start parenting

---
+++ Quals-2020/parenting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int cases;
+    cin >> cases;
+    
+}
+
+int main() {
+    solve();
+    return 0;
+}
